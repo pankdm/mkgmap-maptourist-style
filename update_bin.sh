84666d58ca644465e30864d591375db99b1faf18
Georgy Redkozubov <geshkin@gmail.com>
2012-01-20 21:27:43 +0400

    Checks if files need to be downloaded or not

--- update_bin.sh
+++ update_bin.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
 mkdir -p tmp
+mkgmap_url='http://www.mkgmap.org.uk/snapshots/mkgmap-latest.tar.gz'
+splitter_url='http://www.mkgmap.org.uk/splitter/splitter-r200.tar.gz'
+osmosis_url='http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz'
+mkgmap=`basename $mkgmap_url`
+splitter=`basename $splitter_url`
+osmosis=`basename $osmosis_url`
 
-rm -f mkgmap-latest.tar.gz
-wget -c http://www.mkgmap.org.uk/snapshots/mkgmap-latest.tar.gz
-tar xvfz mkgmap-latest.tar.gz -C tmp
+remote_size=`wget --spider --server-response $mkgmap_url -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'`
+local_size=`ls -l $mkgmap | awk '{ print $5 }'`
+if [ ${remote_size}x != ${local_size}x ]; then
+	rm -f $mkgmap
+	wget -c $mkgmap_url
+fi
+tar xfz $mkgmap -C tmp
 
-rm -f osmosis-latest.tgz
-wget -c http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
-tar xvfz osmosis-latest.tgz -C tmp
+remote_size=`wget --spider --server-response $osmosis_url -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'`
+local_size=`ls -l $osmosis | awk '{ print $5 }'`
+if [ ${remote_size}x != ${local_size}x ]; then
+	rm -f $osmosis
+	wget -c $osmosis_url
+fi
+tar xfz $osmosis -C tmp
 
-rm -f splitter-r200.tar.gz
-wget -c http://www.mkgmap.org.uk/splitter/splitter-r200.tar.gz
-tar xvfz splitter-r200.tar.gz -C tmp
+remote_size=`wget --spider --server-response $splitter_url -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'`
+local_size=`ls -l $splitter | awk '{ print $5 }'`
+if [ ${remote_size}x != ${local_size}x ]; then
+	rm -f $splitter
+	wget -c $splitter_url
+fi
+tar xfz $splitter -C tmp
 
 rm -rf /bin/*.jar
 rm -rf /bin/lib
